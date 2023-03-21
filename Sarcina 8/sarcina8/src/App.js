e51f986402b36c1b23579515b8523b4cc3cd554c
vadogor <vp2010.vadogor@gmail.com>
2023-03-21 13:15:13 +0200

    Sarcina 8 Formular + butonul reset

---
+++ Sarcina 8/sarcina8/src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [productName, setProductName] = useState('');
+  const [price, setPrice] = useState('');
+  const [quantity, setQuantity] = useState('');
+
+  const handleProductNameChange = (event) => {
+    setProductName(event.target.value);
+  };
+
+  const handlePriceChange = (event) => {
+    setPrice(event.target.value);
+  };
+
+  const handleQuantityChange = (event) => {
+    setQuantity(event.target.value);
+  };
+
+  const buttonReset = () => {
+    setProductName('');
+    setPrice('');
+    setQuantity('');
+  };
+
+
+  return (
+    <div className="App">
+      <form>
+        <label>
+          Denumire produs:
+          <input type="text" value={productName} onChange={handleProductNameChange} />
+        </label>
+        <br />
+        <label>
+          Pret pe unitate:
+          <input type="number" value={price} onChange={handlePriceChange} />
+        </label>
+        <br />
+        <label>
+          Cantitate:
+          <input type="number" value={quantity} onChange={handleQuantityChange} />
+        </label>
+        <br />
+        <button type="button">Add Product</button>
+        <button type="button" onClick={buttonReset}>Reset</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
