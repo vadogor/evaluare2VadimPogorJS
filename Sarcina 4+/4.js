63ab660d7ff52364de8c214ccebb7e3c570116a5
vadogor <vp2010.vadogor@gmail.com>
2023-03-21 12:27:27 +0200

    Sarcina 5

--- Sarcina 4+/4.js
+++ Sarcina 4+/4.js
@@ -1,38 +1,53 @@
 class Tour {
-    constructor(name, costOneDay, days, insurance) {
-      this.name = name;
-      this.costOneDay = costOneDay;
-      this.days = days;
-      this.insurance = insurance;
-    }
-  
-    getName() {
-      return this.name;
-    }
-  
-    getCostOneDay() {
-      return this.costOneDay;
-    }
-  
-    getDays() {
-      return this.days;
-    }
-  
-    getInsurance() {
-      return this.insurance;
-    }
-  
-    getPrice() {
-      return this.costOneDay * this.days + this.insurance;
-    }
+  constructor(name, costOneDay, days, insurance) {
+    this.name = name;
+    this.costOneDay = costOneDay;
+    this.days = days;
+    this.insurance = insurance;
   }
-  
-  var tour = new Tour('Turkey', 1500, 7, 750);
-  
-  console.log(tour.getName());
-  console.log(tour.getCostOneDay());
-  console.log(tour.getDays());
-  console.log(tour.getInsurance());
-  console.log(tour.getPrice());
 
-  
+  getName() {
+    return this.name;
+  }
+
+  getCostOneDay() {
+    return this.costOneDay;
+  }
+
+  getDays() {
+    return this.days;
+  }
+
+  getInsurance() {
+    return this.insurance;
+  }
+
+  getPrice() {
+    return this.costOneDay * this.days + this.insurance;
+  }
+
+  increaseDays(numDays) {
+    this.days += numDays;
+  }
+}
+
+console.log(`Sarina 4`);
+var tour = new Tour('Turkey', 1500, 7, 750);
+console.log(tour.getName());
+console.log(tour.getCostOneDay());
+console.log(tour.getDays());
+console.log(tour.getInsurance());
+console.log(tour.getPrice());
+console.log(``);
+
+console.log(`Sarina 5`);
+console.log(tour.getName());
+console.log(tour.getCostOneDay());
+console.log(tour.getDays());
+console.log(tour.getInsurance());
+console.log(tour.getPrice());
+
+tour.increaseDays(3);
+
+console.log(tour.getPrice());
+console.log(``);
